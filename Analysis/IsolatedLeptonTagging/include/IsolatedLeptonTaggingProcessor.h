75c642034e295b00ef9378ec23747a3e90f35302
sss <sss@karma>
2025-09-06 00:24:59 -0400

    Missing namespace qualifiers.

--- Analysis/IsolatedLeptonTagging/include/IsolatedLeptonTaggingProcessor.h
+++ Analysis/IsolatedLeptonTagging/include/IsolatedLeptonTaggingProcessor.h
@@ -25,13 +25,13 @@ public:
 
   /** Called for every run.
    */
-  virtual void processRunHeader(LCRunHeader* run);
+  virtual void processRunHeader(lcio::LCRunHeader* run);
 
   /** Called for every event - the working horse.
    */
-  virtual void processEvent(LCEvent* evt);
+  virtual void processEvent(lcio::LCEvent* evt);
 
-  virtual void check(LCEvent* evt);
+  virtual void check(lcio::LCEvent* evt);
 
   /** Called after data processing for clean up.
    */
@@ -41,7 +41,7 @@ protected:
   /**
    * Add the expected output collections
    */
-  virtual void addOutputColls(LCEvent* evt, LCCollection* pfosWithoutIsoLepColl, LCCollection* isoLepColl);
+  virtual void addOutputColls(lcio::LCEvent* evt, lcio::LCCollection* pfosWithoutIsoLepColl, lcio::LCCollection* isoLepColl);
 
   /** Input collection name.
    */
