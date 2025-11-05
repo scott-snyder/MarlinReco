987ad675dc9f8cb6a1b856397f4a959ed0b6a412
sss <sss@karma>
2025-11-05 13:10:09 -0500

    Refresh of clang

--- Analysis/TauFinder/src/TauFinder.cc
+++ Analysis/TauFinder/src/TauFinder.cc
@@ -498,7 +498,6 @@ bool TauFinder::FindTau(std::vector<ReconstructedParticle*>& Qvec, std::vector<R
       if (_nEvt < coutUpToEv || _nEvt == coutEv)
         streamlog_out(DEBUG) << "Adding Q: " << track->getType() << "\t" << track->getEnergy() << "\t" << p << "\t"
                              << theta << "\t" << phi << std::endl;
-      Etau += Qvec[s]->getEnergy();
       // combine to new momentum
       for (int i = 0; i < 3; i++) {
         pvec_tau[i] = pvec_tau[i] + track->getMomentum()[i];
@@ -530,7 +529,6 @@ bool TauFinder::FindTau(std::vector<ReconstructedParticle*>& Qvec, std::vector<R
         streamlog_out(DEBUG) << "Adding N: " << track->getType() << "\t" << track->getEnergy() << "\t" << p << "\t"
                              << theta << "\t" << phi << std::endl;
 
-      Etau += Nvec[s]->getEnergy();
       // combine to new momentum
       for (int i = 0; i < 3; i++) {
         pvec_tau[i] = pvec_tau[i] + track->getMomentum()[i];
